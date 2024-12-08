4ab5fa9e650f3b9c6977f7f730a8f07a845c824d
bryan <alexandrosavgoustiss@gmail.com>
2024-12-08 16:01:09 +0200

    Create 9.mjs

---
+++ 9.mjs
@@ -0,0 +1,465 @@
+import {$ as e, La as E, Sa as w, W as F, Z as y, aa as f, ba as z, ca as b, da as k, eb as x, g as l, ha as R, k as u, oa as v, qa as _, yb as C} from "./chunk-TB2AAUCW.mjs";
+import "./chunk-JR5VT52U.mjs";
+import {c as o} from "./chunk-Y5FTINFI.mjs";
+var N = "default"in y ? F : y
+  , i = {}
+  , P = N;
+i.createRoot = P.createRoot;
+i.hydrateRoot = P.hydrateRoot;
+var j = i.createRoot
+  , M = i.hydrateRoot;
+var g = {
+    Z7ziArJjs: {
+        elements: {
+            xswPqs7rN: "canvas"
+        },
+        page: e( () => import("./OHukeB441e5CqTvxEzjVmpIS32ys5J8X3-ai_OntpCg.2EOEJK5G.mjs")),
+        path: "/"
+    },
+    OdxS8TCCh: {
+        elements: {
+            jLPR1xMXG: "start-1",
+            pA7gnapzz: "start"
+        },
+        page: e( () => import("./TcMBYp5Dd3POawOe6GQMGnQNvYIt3y50n3CrosVbnKc.SIEDPASH.mjs")),
+        path: "/boostup"
+    },
+    ldyNW0xiA: {
+        elements: {},
+        page: e( () => import("./-QyiFE6viHMEf0VX7lsm2B0KFpnXb3uvU63SQ8PNetA.7Q7TCONQ.mjs")),
+        path: "/boostuplog"
+    },
+    VCW24uMji: {
+        elements: {
+            y_PxqBgA7: "start"
+        },
+        page: e( () => import("./GFeNpYU3NAoa61fhxFxTyCNA3dzI2lrZYYjSoTi5rsA.3ZGBCKVT.mjs")),
+        path: "/boostuppartners"
+    },
+    FzrHrGAP_: {
+        elements: {},
+        page: e( () => import("./Jj9gFd8SzMCbbe48Al_rLM9MWB8lHHl8da_EkO4lTZ8.GU2IRUTA.mjs")),
+        path: "/loadtest"
+    },
+    mzik6Y9yN: {
+        elements: {},
+        page: e( () => import("./_8tsxfqbTsTkHH7egvq_wycOrVxT6Y9oIt3IVvlg_PM.VY64VIBJ.mjs")),
+        path: "/devstrap/home"
+    },
+    vaYNb7Jmr: {
+        elements: {
+            MZ01ObijT: "start"
+        },
+        page: e( () => import("./VNVsCxnFDezq22-C-fv-GjwHFfatAwhX4bRXzRAXhIg.T7P7BHAS.mjs")),
+        path: "/plush"
+    },
+    vJjbHlEde: {
+        elements: {
+            pDYK8UKR1: "start"
+        },
+        page: e( () => import("./RsrvrCX7vRCy-tK4Q3QOMtNQ5IsmttV_OEkMCZY5KVA.F4NORPWH.mjs")),
+        path: "/boostupplus"
+    },
+    uGhWWiq0h: {
+        elements: {},
+        page: e( () => import("./pPTm9V2bcLcsCgNErX2_-ZF-ONcLLCnVdBmBRnVA4q0.CJSRGO6G.mjs")),
+        path: "/fanship"
+    },
+    bkW5iVXgi: {
+        elements: {},
+        page: e( () => import("./IoNaSjq7aznGmJnVSupY4As2S0SvaJ3j7Ivl8BAG1yI.4IWE26JD.mjs")),
+        path: "/lualab"
+    },
+    jgc12oCMM: {
+        elements: {},
+        page: e( () => import("./6OTgzPqFojxydf1-tsW7XeFVAyUASWPE3cWMFCqBGLI.YRGFRN65.mjs")),
+        path: "/swiftpoint"
+    },
+    iHuqraMAR: {
+        elements: {},
+        page: e( () => import("./rpABwsCvzHsgJQpVECZRsPyqp9PxYhypbJ_yOktZU3Q.BPVE5PQX.mjs")),
+        path: "/404"
+    },
+    sDWzuCDT5: {
+        elements: {},
+        page: e( () => import("./wF2G1e51ljCpOBxHodxg0CK5h3C_4fvV2LGwi_dh3uM.LV7ARWWN.mjs")),
+        path: "/ars"
+    },
+    PR7QgPtOZ: {
+        elements: {},
+        page: e( () => import("./_myUc0wAej9BaXJsDlHXogncf61YjXsqSem-_nYzpXI.VAS5T32F.mjs")),
+        path: "/arsweb"
+    },
+    Ovuu5juLz: {
+        elements: {},
+        page: e( () => import("./L92ZzZIhu37U6cqIDUrPu9f7eEyQfYUSljt7xTrQ2oM.3SWD4VK5.mjs")),
+        path: "/kybo"
+    },
+    s2jo0nhcm: {
+        elements: {},
+        page: e( () => import("./Du6iV2zeZ5hI4HYUFhDS8E0UJsDG2UTsE98DR3NFJkk.G5E7PTFG.mjs")),
+        path: "/store"
+    },
+    bL4FXdK6B: {
+        elements: {},
+        page: e( () => import("./o1BNNaHsMcEKCe1fGyHcapyM2ObH-CNvL5WNG5UrNiA.R7KIF4E6.mjs")),
+        path: "/kybo/creator"
+    },
+    YG7IGHSdi: {
+        elements: {},
+        page: e( () => import("./ZB1z7mz-m0OTWewCDpPV4mikrjG8WzzTpjZ9id7FD_o.WCTBIFUN.mjs")),
+        path: "/kybo/insight"
+    },
+    Kn_3txQW8: {
+        elements: {},
+        page: e( () => import("./IN4V1F34oSDTnQujPvhb298Qx0xbB2LwwxT8gACPMGU.IUVM3PEZ.mjs")),
+        path: "/kybo/adpass"
+    },
+    ZzKQHrpi4: {
+        elements: {},
+        page: e( () => import("./eQIGO5xUEqb-E4So85ybjzXKQL3KaCHt9MhUl5Trm-s.MAQLZ24E.mjs")),
+        path: "/parcelbots"
+    },
+    M1ljBp1V3: {
+        elements: {},
+        page: e( () => import("./S-Fj5MHmLpm4JgIBlJ05-WASI8GtVGkhXvfCaJpHC48.FP2ZGHJI.mjs")),
+        path: "/lualabdoc"
+    },
+    EULduLb0D: {
+        elements: {},
+        page: e( () => import("./PzcUACDvWdCNrFSBIbqBcWbCZCGGW-lBruu0R5Y7vTo.JB4HEELB.mjs")),
+        path: "/kybo/troubleshoot"
+    },
+    GvvIQ19XB: {
+        elements: {
+            p2x6mDCvl: "rdcard"
+        },
+        page: e( () => import("./dSyA5ZjsIkT5K3FSv3PiMkF40TiZlu8R0Vk7wkR2KkY.MCZENHTX.mjs")),
+        path: "/rdcbadge"
+    },
+    anoCWBjSc: {
+        elements: {},
+        page: e( () => import("./4ovoZ8xhOA5rS8V6An_m91xkfEbLYJHV8jIp0DIn2Gw.ZCPATW5N.mjs")),
+        path: "/frames/home"
+    },
+    z_dg5tgda: {
+        elements: {},
+        page: e( () => import("./Cuwbfotzg0v9cbqWlQaUd1hlrumuukrnRPkIjkUhLnc.HN6UYE5M.mjs")),
+        path: "/frames/test"
+    },
+    wZBskC0IE: {
+        elements: {},
+        page: e( () => import("./w8Ss5yBev-MiNBQxRoDjPq2SOTyHFqrLs_pHdvZSz_E.ABQY3B6D.mjs")),
+        path: "/frames/success"
+    },
+    sIkojKCKI: {
+        elements: {},
+        page: e( () => import("./KjdUmudy-mEwr8CZdVCH9CMkccE10jVh04C4OQWQN7k.7GWZX2OV.mjs")),
+        path: "/switchup"
+    },
+    Jl16w1ydR: {
+        elements: {},
+        page: e( () => import("./0jwF6dIpWt59qT6idAey0rXdCGSBTm9WZR8pW1s0AK0.K2ZLPGGS.mjs")),
+        path: "/gem/home"
+    },
+    ft8XZSboE: {
+        elements: {},
+        page: e( () => import("./31jpNe6IAAuMFbahTeSPSJwSce7XlC51A7zkhDHLn44.6F6BAFXQ.mjs")),
+        path: "/lualabnew/home"
+    },
+    WKAVTpXPv: {
+        elements: {},
+        page: e( () => import("./cE7AUlkMpPTcuScP4G22OFs_2b59BqY8loD13KW0bSY.K3WC47JO.mjs")),
+        path: "/blog"
+    },
+    iZ1TMMaBZ: {
+        elements: {},
+        page: e( () => import("./BEew4hkD_9qqp5yuKS1NdCJWXOEPCNJc4XOnSIfLh-U.FT2BO4T7.mjs")),
+        path: "/tlc"
+    },
+    jmgLFkb7L: {
+        elements: {},
+        page: e( () => import("./gadk_jC2kB6mVXHxkpo8fhqPx8mRBfRS8W_hretPLYw.I4JHIZFU.mjs")),
+        path: "/kybo/store"
+    },
+    n8CwV6kiB: {
+        elements: {},
+        page: e( () => import("./lk19LhALAncUQEIVZVr0kV-73neloZjWQkV93v8UQro.AYSM6EYG.mjs")),
+        path: "/kybo/ai"
+    },
+    hX9HuIr4S: {
+        elements: {},
+        page: e( () => import("./X-EfSIgTCcgq0r8IspjjwjevlCJsFnqATiesyxFgTnk.KQON3UV3.mjs")),
+        path: "/kybo/climate"
+    },
+    lAwtY8XF8: {
+        elements: {},
+        page: e( () => import("./mwM6xZdi3aTScj5I6fAEenfIqGD3vleUJXZMDQhM8Os.O5XWSOSS.mjs")),
+        path: "/kybo/boards"
+    },
+    uCxk5QPPr: {
+        elements: {},
+        page: e( () => import("./l2eQoTXdQJPB6bJFEDAQz_Zenj54iThNISXbLXzq94M.THMVATJL.mjs")),
+        path: "/kybo/compute"
+    },
+    Cs4N0zE8B: {
+        elements: {},
+        page: e( () => import("./abrknsrNwjk43dWH8MSf-YTOsF8wefqhu1yE4aShabc.YFS6LMMM.mjs")),
+        path: "/kybo/brand"
+    },
+    qiLRJ6wOc: {
+        elements: {},
+        page: e( () => import("./oS62s5q6A1-l-RGXPV2xNmREBziJrgrF15fXaiumW9A.QI4WAYF2.mjs")),
+        path: "/kybo/clocking"
+    },
+    Lv8pjfEDz: {
+        elements: {},
+        page: e( () => import("./Yr3hlnZwJlpq6cc72KHbS1PGaTKZxZGV04qiC9seTqg.J3W24JEW.mjs")),
+        path: "/kybo/hub"
+    },
+    cP7XMXNyz: {
+        elements: {},
+        page: e( () => import("./xN19vCkX8KxDjjNVQtW1AYLeUms9GfR6dZ-w_vHwTnw.Z75ATHJV.mjs")),
+        path: "/canes"
+    },
+    kKv5FWd4e: {
+        elements: {},
+        page: e( () => import("./6OMXmnChRLXXvbH1J5miO0GZXlWWguUpkYqWpkQisVI.W7M5FWT2.mjs")),
+        path: "/canesshop"
+    },
+    VgveLoYNG: {
+        elements: {},
+        page: e( () => import("./g-o7okggrsqoMauggLOXpME7hEmn_tu5M6n8MtuwEmc.YJYQFIY5.mjs")),
+        path: "/cc"
+    },
+    dV54R91g5: {
+        elements: {},
+        page: e( () => import("./1OKrvRbQUqBGNGES-ZZiyM1gNMfMB-S0b6aPMUNpEeg.KBJPRIF4.mjs")),
+        path: "/canecrew"
+    },
+    S32okEe6d: {
+        elements: {},
+        page: e( () => import("./mWiBmO_YsVSL9bGJ7s1nYe9bkbl-clGPTzcmy7-UZHE.ZOQWKJLE.mjs")),
+        path: "/southoak/home"
+    },
+    JsBg_H3PJ: {
+        elements: {},
+        page: e( () => import("./2PalWEIbydx3He5Zf3K4SVK9a0IMeIxCUg6OH-IX3w8.EO5AHO5I.mjs")),
+        path: "/enlua"
+    },
+    ojtKp8fSe: {
+        elements: {},
+        page: e( () => import("./oqr5sqXaCv6aZe4Nqak4vIXD7mnJ-L2w7IKwMzT9jLg.BOTY2KW7.mjs")),
+        path: "/adpass"
+    },
+    LBvLE76cE: {
+        elements: {},
+        page: e( () => import("./-EaI_xyrilli3Ygz56VvoBBgetN6pnDSDgBWNDpjwBk.BCT5LGB2.mjs")),
+        path: "/kybo/recap"
+    },
+    Of_tQVr9H: {
+        elements: {},
+        page: e( () => import("./vjAD0_gq6wx8PPkKnXqnYLqnhvN8Ipx_MScUw4s-CvY.2ZJCVZWP.mjs")),
+        path: "/kybo/recap-2"
+    },
+    RaZbuUH6O: {
+        elements: {},
+        page: e( () => import("./kbZlv6mD-scwhMlrV80ChjrNesJex_vZZg_qLzMJ4XQ.ZYAZXMZ6.mjs")),
+        path: "/kybo/recap-3"
+    },
+    gZZf1d6WN: {
+        collectionId: "VsZ_g3RrX",
+        elements: {
+            cXy95KH5C: "bottom",
+            fERAogmdaWKAz_h3vMUABi_Ou_M: "top"
+        },
+        page: e( () => import("./QXErvkSJL3uHNaYfeZh29eZCoblcDa4Cpzksav3n8Y0.DULEK32E.mjs")),
+        path: "/blog/:NxzQ67UkF"
+    },
+    OnfYB0SzZ: {
+        collectionId: "AN1mspjgm",
+        elements: {},
+        page: e( () => import("./2JAysKdACXwMZkjyRpcaNQp9yvPeGvRefAMvqef6dkU.46L3VQBY.mjs")),
+        path: "/files/:riTe3S3KY"
+    }
+}
+  , I = [{
+    code: "en",
+    id: "default",
+    name: "English",
+    slug: ""
+}];
+async function A({routeId: p, pathVariables: s, localeId: t}) {
+    await g[p].page.preload();
+    let m = l(w, {
+        isWebsite: !0,
+        routeId: p,
+        pathVariables: s,
+        routes: g,
+        collectionUtils: {
+            AN1mspjgm: async () => (await import("./5GYMcN8cMb7UE1oOQMGabar5AGiBB9Lq2wV-Qn_EOug.OYXPS7O6.mjs"))?.utils,
+            VsZ_g3RrX: async () => (await import("./eFnfnjoIIWsGuEiX0B1BV2UB1-i2FhdzpB_FAHu8tkg.7VHWYBEG.mjs"))?.utils
+        },
+        framerSiteId: "29cc26f1e027929a71493ed8742f9ede667972cd916537b13b9c183597d01d7b",
+        notFoundPage: e( () => import("./rpABwsCvzHsgJQpVECZRsPyqp9PxYhypbJ_yOktZU3Q.BPVE5PQX.mjs")),
+        isReducedMotion: void 0,
+        localeId: t,
+        locales: I,
+        preserveQueryParams: void 0
+    })
+      , n = l(E, {
+        children: m,
+        value: {
+            enableAsyncURLUpdates: !0,
+            replaceNestedLinks: !0,
+            useGranularSuspense: !0,
+            wrapUpdatesInTransitions: !1
+        }
+    })
+      , r = {
+        enter: {
+            mask: {
+                angle: 34,
+                type: "wipe",
+                width: "0%"
+            },
+            opacity: 0,
+            rotate: 0,
+            rotate3d: !1,
+            rotateX: 0,
+            rotateY: 0,
+            scale: 1,
+            transition: {
+                damping: 60,
+                delay: 0,
+                duration: .2,
+                ease: [.27, 0, .51, 1],
+                mass: 1,
+                stiffness: 500,
+                type: "spring"
+            },
+            x: "0px",
+            y: "0px"
+        },
+        exit: {
+            mask: {
+                angle: 34,
+                type: "wipe",
+                width: "0%"
+            },
+            opacity: 0,
+            rotate: 0,
+            rotate3d: !1,
+            rotateX: 0,
+            rotateY: 0,
+            scale: .5,
+            transition: {
+                damping: 60,
+                delay: 0,
+                duration: .2,
+                ease: [.27, 0, .51, 1],
+                mass: 1,
+                stiffness: 500,
+                type: "spring"
+            },
+            x: "0px",
+            y: "0px"
+        }
+    };
+    return l(f, {
+        children: n,
+        value: {
+            routes: {
+                ldyNW0xiA: {
+                    VCW24uMji: r,
+                    vJjbHlEde: r
+                },
+                OdxS8TCCh: {
+                    VCW24uMji: r,
+                    vJjbHlEde: r
+                },
+                VCW24uMji: {
+                    OdxS8TCCh: r
+                },
+                vJjbHlEde: {
+                    OdxS8TCCh: r,
+                    VCW24uMji: r
+                }
+            }
+        }
+    })
+}
+var W = typeof document < "u";
+if (W) {
+    o.__framer_importFromPackage = (s, t) => () => l(v, {
+        error: 'Package component not supported: "' + t + '" in "' + s + '"'
+    }),
+    o.process = {
+        ...o.process,
+        env: {
+            ...o.process ? o.process.env : void 0,
+            NODE_ENV: "production"
+        }
+    },
+    o.__framer_events = o.__framer_events || [],
+    _();
+    let p = document.getElementById("main");
+    "framerHydrateV2"in p.dataset ? V(!0, p) : V(!1, p)
+}
+function S() {
+    W && o.__framer_events.push(arguments)
+}
+async function V(p, s) {
+    try {
+        let h = function(a, H) {
+            let d = H?.componentStack;
+            console.warn("Recoverable error during hydration. Please check any custom code or code overrides to fix server/client mismatches.", a, d),
+            !(Math.random() > .01) && S("published_site_load_recoverable_error", {
+                message: String(a),
+                componentStack: d,
+                stack: d ? void 0 : a instanceof Error && typeof a.stack == "string" ? a.stack : null
+            })
+        }, t, m, n, r;
+        if (p) {
+            let a = JSON.parse(s.dataset.framerHydrateV2);
+            t = a.routeId,
+            m = a.localeId,
+            n = a.pathVariables,
+            r = a.breakpoints
+        } else {
+            let a = R(g, decodeURIComponent(location.pathname), !0, I);
+            t = a.routeId,
+            m = a.localeId,
+            n = a.pathVariables
+        }
+        let c = await A({
+            routeId: t,
+            localeId: m,
+            pathVariables: n
+        });
+        p ? (C("framer-rewrite-breakpoints", () => {
+            x(r),
+            o.__framer_onRewriteBreakpoints?.(r)
+        }
+        ),
+        u( () => {
+            z(),
+            k(),
+            b(),
+            M(s, c, {
+                onRecoverableError: h
+            })
+        }
+        )) : j(s, {
+            onRecoverableError: h
+        }).render(c)
+    } catch (t) {
+        throw S("published_site_load_error", {
+            message: String(t),
+            stack: t instanceof Error && typeof t.stack == "string" ? t.stack : null
+        }),
+        t
+    }
+}
+export {A as getPageRoot};
+//# sourceMappingURL=script_main.KXWYAVVM.mjs.map
