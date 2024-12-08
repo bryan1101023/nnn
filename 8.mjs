1fb60d72409373267da5c4006994b0851dd4c69b
bryan <alexandrosavgoustiss@gmail.com>
2024-12-08 15:59:16 +0200

    Create 8.mjs

---
+++ 8.mjs
@@ -0,0 +1,11 @@
+function a(e, t) {
+    return {
+        customHTMLHeadStart: "<style>   /* Hide scrollbar for Chrome, Safari and Opera */   body::-webkit-scrollbar {     display: none;   }    /* Hide scrollbar for IE, Edge and Firefox */   body {     -ms-overflow-style: none;  /* IE and Edge */     scrollbar-width: none;  /* Firefox */   } </style>",
+        favicon: "https://framerusercontent.com/assets/bX6VF88EdFQjyNYGQcsEoTS7D9A.png",
+        robots: "max-image-preview:large",
+        socialImage: "https://framerusercontent.com/assets/WEfDj2L5zyruEuzTqsFWttSbU.png",
+        title: "Julian"
+    }
+}
+export {a};
+//# sourceMappingURL=chunk-YCE52UMC.mjs.map
